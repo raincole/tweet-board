a19e828573a69f6aa71e1c597ed2da5d2f98212b
raincole <raincolee@gmail.com>
2013-05-24 21:26:59 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TweeBoard::Application.config.secret_key_base = '43afb4f1f4456cb44933171527d88cbefcb0da8e1725eaa73d7b4c0dc984a2efd2d45692db0899873807633a4debc34774a1fc25e3d0547ba8dbaf873dcc1c81'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TweeBoard::Application.config.session_store :cookie_store, key: '_twee_board_session'
