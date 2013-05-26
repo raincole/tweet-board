bc21e87f21bffe449e07860eed49bde2fd49e57f
raincole <raincolee@gmail.com>
2013-05-26 16:22:26 +0800

    Add SubscriptionsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TweeBoard::Application.routes.draw do
+  get "subscriptions/index"
   resources :subscriptions do
     resource :tweets, :only => :index
   end
@@ -10,7 +11,7 @@ TweeBoard::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'subscriptions#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
